9c11dc1c725d2daee4d2a7288f47c4f587f1ae42
mariumfaisal23 <mariumfaisal66@gmail.com>
2024-08-05 15:56:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// AOS.init();
+// let menu = document.querySelector("#menu")
+// let bars= document.querySelector("#bars")
+
+// bars.addEventListener("click", function(){
+//     menu.classList.toggle("mobilemenu")
+
+// })
+// var swiper = new swiper(".mySwiper",{
+//     navigation:{
+//         nextE1:".next",
+//         prevE1:".prev",
+//         autoplay:".true"
+//     },
+// });
+
